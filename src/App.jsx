6c64905875fee05269039ba7432c074b251fbbfb
Guilherme Costa <echina725@gmail.com>
2024-01-22 14:51:06 -0300

    refactoring project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   return (
-    <>
-      Hello
-    </>
+    <BrowserRouter>
+        <header className='flex flex-row space-x-10'>
+            <h2>Header goes here</h2>
+            <div>
+                <Link to='/'>Home</Link>
+                <Link to='/about'>About</Link>
+            </div>
+        </header>
+        <Routes>
+            <Route path='/' element={<Home />}/>
+            <Route path='/about'element={<About />} />
+        </Routes>
+    </BrowserRouter>
   )
 }
 
