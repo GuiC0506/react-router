3eda5a81ef7b7768ad5acd11047010a7ebd18713
Guilherme Costa <echina725@gmail.com>
2024-01-24 00:21:34 -0300

    search params

--- src/pages/BookLayout.jsx
+++ src/pages/BookLayout.jsx
@@ -1,17 +1,22 @@
-import React from "react";
-import { Outlet, Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Outlet, Link, useSearchParams } from "react-router-dom";
 //Outlet: renders nested elements inside that parent route.
 
 export default function BookLayout() {
+    const [searchParams, setSearchParams] = useSearchParams({n: 3});
+    const number = searchParams.get("n");
     return (
         <>
             <Link to="/books/1">Book 1</Link>
             <br />
             <Link to="/books/2">Book 2</Link>
             <br />
-            <Link to="/books/new">New Book</Link>
+            <Link to={`/books/${number}`}>Book {number}</Link>
+            <br />
+            <Link to={`/books/new`}>New Book</Link>
             <br />
             <Outlet context={["username", "age"]}/>
+            <input type="number" value={number} onChange={e => setSearchParams({n: e.target.value})}/>
         </>
         
     )
